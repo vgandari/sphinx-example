99c489bc325da9895aee2a532c09a88a4ed99a04
vgandari <victor.gandarillas@icloud.com>
2020-11-16 13:14:09 -0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,7 @@
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
-from openmdao.docutils import embed_code, embed_options, do_monkeypatch
-do_monkeypatch()
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
